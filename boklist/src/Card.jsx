7930cec847d736e0ff7db00eae23ea389a24db7c
jaden van schalkwyk <vanschalkwykjaden10@gmail.com>
2025-03-07 08:30:55 +0200

    Added number of cards

--- boklist/src/Card.jsx
+++ boklist/src/Card.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
+import React, { useState } from "react";
 import './index.css'
 
 
 function Card(props){
-    
+    const number = useState(0);
     return(
         <>
         <div className="card">
@@ -11,6 +12,7 @@ function Card(props){
             <img className="cardImage" src={props.img} alt="a boook of brad taylor"></img>
             <h2> {props.title} </h2>
             <p> {props.about} </p>
+            <p> {number + 1} </p>
             </div>
         </div>
       
